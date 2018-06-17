e954dc12fc3f476d109b2fb75bad336884c048fa
Rair Angelos <rairangelos@gmail.com>
2018-06-16 21:08:06 -0300

    site funcional

---
+++ email.php
@@ -0,0 +1,49 @@
+<?php
+$Nome		= $_POST["Nome"];	// Pega o valor do campo Nome
+$Fone		= $_POST["Fone"];	// Pega o valor do campo Telefone
+$Email		= $_POST["Email"];	// Pega o valor do campo Email
+$Mensagem	= $_POST["Mensagem"];	// Pega os valores do campo Mensagem
+
+// Variável que junta os valores acima e monta o corpo do email
+
+$Vai 		= "Nome: $Nome\n\nE-mail: $Email\n\nTelefone: $Fone\n\nMensagem: $Mensagem\n";
+
+require_once("phpmailer/class.phpmailer.php");
+
+define('GUSER', 'contato@rsprimonegocios.com.br');	// <-- Insira aqui o seu GMail
+define('GPWD', 'Ybj72lCeP6f8');		// <-- Insira aqui a senha do seu GMail
+
+function smtpmailer($para, $de, $de_nome, $assunto, $corpo) { 
+	global $error;
+	$mail = new PHPMailer();
+	$mail->IsSMTP();		// Ativar SMTP
+	$mail->SMTPDebug = 1;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
+	$mail->SMTPAuth = true;		// Autenticação ativada
+	$mail->SMTPSecure = 'ssl';	// SSL REQUERIDO pelo GMail
+	$mail->Host = 'mx1.hostinger.com.br';	// SMTP utilizado
+	$mail->Port = 465;  		// A porta 587 deverá estar aberta em seu servidor
+	$mail->Username = GUSER;
+	$mail->Password = GPWD;
+	$mail->SetFrom($de, $de_nome);
+	$mail->Subject = $assunto;
+	$mail->Body = $corpo;
+	$mail->AddAddress($para);
+	if(!$mail->Send()) {
+		$error = 'Mail error: '.$mail->ErrorInfo; 
+		return false;
+	} else {
+		header('Location: index.php ');
+		return true;
+	}
+}
+
+// Insira abaixo o email que irá receber a mensagem, o email que irá enviar (o mesmo da variável GUSER), 
+//o nome do email que envia a mensagem, o Assunto da mensagem e por último a variável com o corpo do email.
+
+ if (smtpmailer('contato@rsprimonegocios.com.br', 'contato@rsprimonegocios.com.br', 'RS - Primo Negócios', 'Contato - Fale Conosco', $Vai)) {
+
+	Header('Location: index.php'); // Redireciona para uma página de obrigado.
+
+}
+if (!empty($error)) echo $error;
+?>
---
+++ index.php
@@ -0,0 +1,242 @@
+﻿<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>RS PRIMO NEGÓCIOS</title>
+	<!-- Última versão CSS compilada e minificada -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Tema opcional -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!--Style CSS Personalizado -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!--Ícone Guia -->
+	<link rel="shortcut icon" href="img/icon.png" type="image/x-icon" />
+
+	<!--[if lt IE 9]>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+	<![endif]-->
+<meta name="google-site-verification" content="WzqUljU9eOlcYTN67TE9A1OBuShoNlUt7cqE_0TSCi4" />
+</head>
+	<body>
+		<header class="header">
+			<h1 class="oculta">Rs Primo Negócios</h1>
+			<div class="logo">
+				<img class="logo" src="img/logo.png" alt="Rs Primo Negócios" title="Rs Primo Negócios">
+				<h1 class="text-logo1">RS </h1> <h1 class="text-logo2"> Primo Negócios</h1>
+			</div>
+			<div class="container hidden-sm hidden-xs">
+				<nav>
+					<ul class="menu">
+						<li>
+							<a href="#sobre">SOBRE A RS</a>
+						</li>
+						<li>
+							<a href="#sobre">MISSÕES E VALORES</a>
+						</li>
+						<li>
+							<a href="#servicos">SERVIÇOS</a>
+						</li>
+						<li>
+							<a href="#clientes">PRINCIPAIS CLIENTES</a>
+						</li>
+						<li>
+							<a href="#contatos">CONTATO</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+		<!--- Fim da HEADER -->
+
+		<div class="clear"></div>
+
+		<section class="slide text-center background-parallax-slide color-white">
+
+				<div class="container bground">
+					<h1 class="title-slide color-title">UMA EMPRESA ESPECIALISTA EM CARGA E DESCARGA DE MERCADORIAS E  PRODUTOS</h1> 
+				</div>
+
+
+		</section>
+		<!--- Fim SECTION slide -->
+
+		<section class="sobre padding-20" id="sobre">
+			<div class="container text-center">
+				<div class="container padding-20">
+					<h1 class="title">Sobre a RS</h1>
+					<article class="col-md-4 detalhe-sobre">
+						<h1 class="oculta">Rosinaldo</h1>
+						<img class="img-sobre" src="img/group.png" alt="História" title="História">
+					</article>
+					<article class="col-md-8 detalhe-sobre">
+						<h1 class="oculta">A RS Primo Negócios</h1>
+
+						<p class="descricao2 text-justify">
+							Desde 2011 fazendo logística em diversas empresas da região metropolitana e do grande ABCD em São Paulo e Rio de Janeiro com profissionais qualificados e alta perfomance com eficácia objetivando viabilizar processos e serviços.
+						</p>
+					</article>
+				</div>
+				<div class="container">
+				<article class="col-md-6 detalhe-sobre">
+					<h1 class="title2">Missão</h1>
+					<p class="missao_valor"> 
+						Satisfazer os clientes com desempenho e eficácia.
+					</p>
+				</article>
+				<article class="col-md-6 detalhe-sobre">
+					<h1 class="title2">Valor</h1>
+					<p class="missao_valor"> 
+						Preservar a vida e o meio ambiente tendo a vida como bem maior.
+					</p>
+				</article>
+				</div>
+			</div>
+		</section>
+		<!--- Fim SECTION sobre -->
+
+
+		<section class="metodo padding-60 metodo" id="servicos">
+			<div class="container-fluid text-center">
+				<h1 class="title-about1">Serviços</h1>
+
+				<article class="col-md-2 detalhe-sobre">
+					<img class="img-metodo" src="img/carts.png" alt="Aprender a Fazer" title="Aprender a Fazer">
+					<p class="about1">SERVIÇO DE CARGA E DESCARGA DE MERCADORIAS E BAGAGENS.</p>
+				</article>
+
+				<article class="col-md-2 detalhe-sobre">
+					<img class="img-metodo" src="img/customer.png" alt="Aprender a Ser" title="Aprender a Ser">
+					<p class="about1">FORNECIMENTO DE PESSOAL DE APOIO PARA PRESTAÇÃO DE SERVIÇO (MANUTENÇÕES).</p>
+				</article>
+
+				<article class="col-md-2 detalhe-sobre">
+					<img class="img-metodo" src="img/cleaner.png" alt="Aprender a Ser" title="Aprender a Ser">
+					<p class="about1">SERVIÇO DE LIMPEZA GERAL EM INTERIORES DE PRÉDIOS E EDÍFICIOS.</p>
+				</article>
+				
+				<article class="col-md-2 detalhe-sobre">
+					<img class="img-metodo" src="img/conveyor.png" alt="Aprender a Ser" title="Aprender a Ser">
+					<p class="about1">SERVIÇOS DE COORDENAÇÃO E DESENVOLVIEMNTO DE PROJETOS LOGÍSTICOS.</p>
+				</article>
+								
+				<article class="col-md-2 detalhe-sobre">
+					<img class="img-metodo" src="img/working.png" alt="Aprender a Ser" title="Aprender a Ser">
+					<p class="about1">SERVIÇOS ADMINISTRATIVOS PARA EMPRESAS.</p>
+				</article>
+				
+				<article class="col-md-2 detalhe-sobre">
+					<img class="img-metodo" src="img/reception.png" alt="Aprender a Ser" title="Aprender a Ser">
+					<p class="about1">SERVIÇOS DE RECEPÇÃO E PORTARIA.</p>
+				</article>
+			</div>
+		</section>
+		<!--- Fim SECTION metodo -->
+
+
+		<section class="objetivo padding-60 " id="clientes">
+				<div class="container-fluid text-center">
+				<h1 class="title-about2">Principais Clientes</h1>
+
+				<article class="col-md-3 detalhe-sobre">
+					<img class="img-objetivo" src="img/Logo_PortoNovaEra.png" alt="" title="">
+				</article>
+			
+				<article class="col-md-2 detalhe-sobre ">
+					<img class="img-objetivo vda" src="img/VDA.png" alt="" title="">
+				</article>
+				
+				<article class="col-md-3 detalhe-sobre">
+					<img class="img-objetivo brida" src="img/BRIDA.jpg" alt="" title="">
+				</article>
+
+				<article class="col-md-2 detalhe-sobre">
+					<img class="img-objetivo njg" src="img/NJG.jpg" alt="" title="">
+				</article>
+				
+				<article class="col-md-2 detalhe-sobre">
+					<img class="img-objetivo njg" src="img/hiato.jpeg" alt="" title="">
+				</article>
+
+			</div>
+		</section>
+		<!--- Fim SECTION Clientes -->
+
+
+		<section class="rodape1 padding-60" id="contatos">
+			<div class="container-fluid text-center">
+				<h1 class="title-about1 localiza">Contato</h1>
+				<div class="container-fluid">
+
+					<div class="col-md-3 text-left">
+						<div class="container col-md-12">
+							<h4 class="color-white" style="font-weight: bolder;">RS PRIMO NEGOCIOS</h4>	
+							<h6 class="color-white">CNPJ: 17.019.255/0001-07</h6>	
+							<h6 class="color-white">Rua Caçaquera, 39 <br>Bairro: Vila Antonina<br>São Paulo/SP</h6>
+							<h6 class="color-white">contato@rsprimonegocios.com.br</h6>
+							<h6 class="color-white">+55 11 99619-5637 (Vivo)</h6>
+							<h6 class="color-white">+55 11 94034-3303 (NEXTEL)</h6>
+							<h6 class="color-white">101*212543 (Rádio)</h6>
+							<a href="#"><img class="" src="img/facebook-logo.png" alt="" title=""></a>
+							<a href="#"><img class="" src="img/linkedin-logo.png" alt="" title=""></a>
+							<a href="#"><img class="" src="img/twitter-logo.png" alt="" title=""></a>
+						</div>
+					</div>
+
+					<iframe class="col-md-5" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1828.711850735554!2d-46.55347378519838!3d-23.553221512694208!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5e7c2b54f6c9%3A0xe227f7bf77b0dd07!2sR.+Ca%C3%A7aquera%2C+39+-+Vila+Antonina%2C+S%C3%A3o+Paulo+-+SP%2C+03412-030!5e0!3m2!1spt-BR!2sbr!4v1519069591773" width="600" height="450" frameborder="0" style="border:0" allowfullscreen>
+					</iframe>
+
+					<div class="col-md-4">
+						<form class="form-group" method="POST" action="email.php">
+					        <h5 class="color-white" style="font-weight: bolder;">FALE CONOSCO</h5>
+				            <input class="form-control" type="text" id="nomeid" placeholder="Nome" required="required" name="Nome" />
+					        <label for="nome"></label>
+					        <input class="form-control" type="text" id="foneid" placeholder="Telefone com DDD" name="Fone" />
+					        <label for="fone"></label>
+					        <input class="form-control" type="email" id="emailid" placeholder="E-mail" name="Email" />
+					        <label for="email"></label>
+					        <textarea class="form-control" placeholder="Fale conosco" name="Mensagem"></textarea>
+					        <br>
+					        <input class="btn btn-danger" type="submit" value="Enviar" />
+					    </form>
+					</div>
+
+				</div>
+			</div>
+		</section>
+		<!--- Fim SECTION Localização -->
+
+
+		<section class="rodape2 padding-10" id="rodape">
+			<div class="container-fluid text-center">
+				<div class="rodape2"><h6>Todos os direitos reservados ® 2017. Desenvolvido por <a href="http://www.rairangelos.com">Rair Angelos</a></h6></div>
+			</div>
+		</section>
+
+		<!--- Fim SECTION rodapé -->
+
+		<!--- JQuery -->
+		<script src="js/jquery-3.2.1.slim.min.js"></script>
+		<!-- Última versão JavaScript compilada e minificada -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [{
+    "@type": "ListItem",
+    "position": 1,
+    "item": {
+      "@id": "https://rsprimonegocios.com.br/",
+      "name": "RS PRIMO NEGÓCIOS",
+      "image": "https://rsprimonegocios.com.br/img/template.jpg"
+    }
+  }]
+}
+</script>
+	</body>
+</html>
